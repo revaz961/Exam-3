b202dd4ed8defeb320bb190a11e4abc227171d62
revaz <revaz961@gmail.com>
2021-05-22 23:10:27 +0400

    30 min after the exam

--- app/src/main/java/com/example/exam3/UserActivity.kt
+++ app/src/main/java/com/example/exam3/UserActivity.kt
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 
 class UserActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         const val REQUEST_ACCESS_TYPE = 1
         const val REQUEST_MODIFY_TYPE = 2
         const val ACCESS_MESSAGE = "user"
@@ -35,15 +35,17 @@ class UserActivity : AppCompatActivity() {
                 if (resultCode == RESULT_OK) {
                     val user = data?.getParcelableExtra<User>(ACCESS_MESSAGE) as User
                     users.add(user)
-                    adapter.notifyDataSetChanged()
+//                    adapter.notifyDataSetChanged()
+                    adapter.notifyItemInserted(users.size - 1)
                 }
             }
             REQUEST_MODIFY_TYPE -> {
                 if (resultCode == RESULT_OK) {
                     val user = data?.getParcelableExtra<User>(MODIFY_MESSAGE) as User
-                    if(users[modifyPosition] != user) {
+                    if (modifyPosition != -1 && users[modifyPosition] != user) {
                         users[modifyPosition] = user
                         adapter.notifyItemChanged(modifyPosition)
+                        modifyPosition = -1
                     }
                 }
             }
@@ -51,47 +53,50 @@ class UserActivity : AppCompatActivity() {
         }
     }
 
-    private fun init(){
+    private fun init() {
+        setData()
+        rvUsers = findViewById(R.id.rvUsers)
         btnAdd = findViewById(R.id.btnAdd)
         btnClear = findViewById(R.id.btnClear)
+
         btnAdd.setOnClickListener {
             val intent = Intent(this, AddUserActivity::class.java)
-            startActivityForResult(intent,REQUEST_ACCESS_TYPE)
+            startActivityForResult(intent, REQUEST_ACCESS_TYPE)
         }
 
         btnClear.setOnClickListener {
-            if(users.isNotEmpty()) {
+            if (users.isNotEmpty()) {
                 adapter.notifyItemRangeRemoved(0, users.size)
                 users.clear()
             }
         }
 
-        rvUsers = findViewById(R.id.rvUsers)
-        setData()
-
-        adapter = UserRecyclerAdapter(users,{
+        adapter = UserRecyclerAdapter(users, {
             adapter.notifyItemRemoved(it)
             users.removeAt(it)
-        }){
+        }) {
             modifyPosition = it
             val intent = Intent(this, UserModifyActivity::class.java)
-            intent.putExtra(MODIFY_MESSAGE,users[it])
+            intent.putExtra(MODIFY_MESSAGE, users[it])
             startActivityForResult(intent, REQUEST_MODIFY_TYPE)
         }
-        rvUsers.layoutManager = GridLayoutManager(this,2)
+
+        rvUsers.layoutManager = GridLayoutManager(this, 2)
         rvUsers.adapter = adapter
     }
 
-    private fun setData(){
-        users.addAll(listOf(
-            User("Revaz","Giorgadze","revaz961@gmail.com"),
-            User("Nika","Mamukelashvili","Nika961@gmail.com"),
-            User("Giorgi","Odishvili","Giorgi961@gmail.com"),
-            User("Mari","Samniashvili","Mari961@gmail.com"),
-            User("Nino","Mamacashvili","Nino961@gmail.com"),
-            User("Levani","Zakeridze","Levani961@gmail.com"),
-            User("Robi","Mchedlishvili","Robi961@gmail.com"),
-            User("Natia","Gagnidze","Natia961@gmail.com"),
-        ))
+    private fun setData() {
+        users.addAll(
+            listOf(
+                User("Revaz", "Giorgadze", "revaz961@gmail.com"),
+                User("Nika", "Mamukelashvili", "Nika961@gmail.com"),
+                User("Giorgi", "Odishvili", "Giorgi961@gmail.com"),
+                User("Mari", "Samniashvili", "Mari961@gmail.com"),
+                User("Nino", "Mamacashvili", "Nino961@gmail.com"),
+                User("Levani", "Zakeridze", "Levani961@gmail.com"),
+                User("Robi", "Mchedlishvili", "Robi961@gmail.com"),
+                User("Natia", "Gagnidze", "Natia961@gmail.com"),
+            )
+        )
     }
 }
